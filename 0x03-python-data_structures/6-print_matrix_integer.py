b3eb8a91f8cfe6b3c1bd62fbfc26474822474c31
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-04 22:56:29 +0000

    Correct error of wrong output

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers"""
-    for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=" ")
+    for num_list in matrix:
+        for i in range(len(num_list)):
+            if i != (len(num_list) -1):
+                print("{:d} ".format(num_list[i]), end="")
+            else:
+                print("{:d}".format(num_list[i]), end="")
         print()
