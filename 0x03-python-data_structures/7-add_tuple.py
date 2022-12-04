f0e789c586f8ae75cbe9650b7c30f4ae9a43705d
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-04 23:37:43 +0000

    Remove whitespace at line 12

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,7 +9,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return tuple_a
     if len(tuple_a) == 0:
         return tuple_b
-    if len(tuple_a) < 2 and len(tuple_b) < 2 :
+    if len(tuple_a) < 2 and len(tuple_b) < 2:
         return (tuple_a[0] + tuple_b[0], 0)
     if len(tuple_a) < 2:
         return (tuple_a[0] + tuple_b[0], tuple_b[1])
