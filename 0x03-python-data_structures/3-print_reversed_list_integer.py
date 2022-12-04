d8ed3252e30cf9d6f0c50bc4586cbe2b55863105
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-04 19:30:50 +0000

    Check for length of list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """prints all integers of a lis, in reverse order"""
+    if len(my_list) == 0:
+        return
     my_list.reverse()
     for num in my_list:
         print("{:d}".format(num))
