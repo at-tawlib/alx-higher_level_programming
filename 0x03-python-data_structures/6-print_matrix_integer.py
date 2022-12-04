070c24e5dab43d895bafa9440e7b4a30ddf8c5d3
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-04 23:02:01 +0000

    Handle pycodestyle error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers"""
     for num_list in matrix:
         for i in range(len(num_list)):
-            if i != (len(num_list) -1):
+            if i != (len(num_list) - 1):
                 print("{:d} ".format(num_list[i]), end="")
             else:
                 print("{:d}".format(num_list[i]), end="")
