626280e60932cf7f19178863ea3b78ed833cb5be
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-04 23:29:07 +0000

    Handle if both are 0

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     """adds two tuples and returns a tuple with two integers
-    the first element is the addition of the first element of each argument
-    the second element should be the addition of the second element of each argument"""
+    the first element is the sum of the first element of each argument
+    the second element is the sum of the second element of each argument"""
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return (0, 0)
     if len(tuple_b) == 0:
         return tuple_a
     if len(tuple_a) == 0:
