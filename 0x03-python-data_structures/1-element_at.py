fb899566cca7fffffd0dfd5e07e447b81f2ab486
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-04 18:38:44 +0000

    Change None string to None

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    if len(my_list) == 0:
+        return None
     if idx < 0:
-        return "None"
+        return None
     if idx > len(my_list):
-        return "None"
+        return None
     return my_list[idx]
 
