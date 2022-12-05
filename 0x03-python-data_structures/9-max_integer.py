311116a48e3491a034d766c1eb2cadd0bddc71d8
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-05 03:19:12 +0000

    Create 9-max_integer.py and 9-main.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """finds the biggest integer of a list"""
+    if len(my_list) == 0:
+        return None
+    max_int = 0
+    for num in my_list:
+        if num > max_int:
+            max_int = num
+    return max_int
