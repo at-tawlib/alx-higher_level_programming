964aba325a6dd43d665227d03865c51d50b8df64
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-08 02:29:41 +0000

    Pycodestyle complaint

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """returns a key with the biggest integer value"""
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     biggest_int = 0
     biggesst_key = None
@@ -9,4 +9,4 @@ def best_score(a_dictionary):
         if a_dictionary[key] > biggest_int:
             biggest_int = a_dictionary[key]
             biggest_key = key
-    return biggest_key 
+    return biggest_key
