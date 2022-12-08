ddafa79b1e922e2004c50510b8db2c76a216c9be
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-08 02:27:52 +0000

    Create 10-best_score.py and 10-main.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """returns a key with the biggest integer value"""
+    if a_dictionary == None:
+        return None
+    biggest_int = 0
+    biggesst_key = None
+    for key in a_dictionary:
+        if a_dictionary[key] > biggest_int:
+            biggest_int = a_dictionary[key]
+            biggest_key = key
+    return biggest_key 
