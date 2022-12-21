5e177c9b4c7e6a805efbc9bb3eaa9248a643d45c
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-21 06:02:25 +0000

    udpate again

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -68,7 +68,7 @@ class Square:
         """
         if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value != 2):
+        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
