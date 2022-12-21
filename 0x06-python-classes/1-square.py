40ded2ebcb584e02fe956801c2eb84c1b424295c
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-21 03:14:54 +0000

    Create 1-square.py and 1-main.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Definition of a Square"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+            __size (int): size of a side of the square
+    """
+
+    def __init__(self, size):
+        """Initialize a square
+        Args:
+            size (int): size fo a side of the square
+        """
+
+        self.__size = size
