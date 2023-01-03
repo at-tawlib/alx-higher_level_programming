1e4c9d9236b1f8a93861694fef62575b0075ecf8
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-03 21:57:36 +0000

    Update file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -19,16 +19,8 @@ class Rectangle:
             ValueError: if width or height is less than zero
         Return: None
         """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -92,4 +84,6 @@ class Rectangle:
         Return:
             perimeter of a rectangle
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
