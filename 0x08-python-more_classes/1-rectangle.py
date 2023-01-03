30f4503ff4e607ff138bbc0e8ba74ab393838b91
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-03 21:57:18 +0000

    Update code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,16 +19,8 @@ class Rectangle:
             ValueError: if width or height is less than zero
         Return: None
         """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
