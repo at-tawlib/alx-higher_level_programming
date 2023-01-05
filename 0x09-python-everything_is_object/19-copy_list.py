3097d74df15b6ed63fec42a0f97542e0bdf7701f
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-05 02:55:56 +0000

    Create 19-copy_list.py and 19-main.py

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def copy_list(l):
+    """Returns a copy of a list
+    Args:
+        l (list): list to copy
+    Return:
+        copy of list
+    """
+    new_list = l[:]
+    return new_list
