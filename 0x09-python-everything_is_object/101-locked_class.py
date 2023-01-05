40d3d2bfd5afbf3ede09399ec4903f20b8c24885
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-05 05:43:45 +0000

    Pycodestyle udpated

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This defines a locked class"""
 
+
 class LockedClass:
     """Allows instantiaion of attribute 'first_name' and nothing else"""
     __slots__ = ["first_name"]
