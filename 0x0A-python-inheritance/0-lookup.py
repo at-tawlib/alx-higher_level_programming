bcb5d5e872e49ac0f797b7d837867d82ad7ed9ee
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-09 19:03:02 +0000

    Create 0-... files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+list of attributes and methods of an object
+"""
+
+def lookup(obj):
+    """returns a the list of attributes and methods of an object
+    Args:
+        obj (object): object to check    
+    """
+    print(dir(obj));
