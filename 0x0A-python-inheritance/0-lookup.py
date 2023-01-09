dc16195fea2d6027f04f2be2410f4ec138b2087d
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-09 19:11:42 +0000

    Pycodestyle compliant

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,9 +3,10 @@
 list of attributes and methods of an object
 """
 
+
 def lookup(obj):
     """returns a the list of attributes and methods of an object
     Args:
-        obj (object): object to check    
+        obj (object): object to check
     """
     return dir(obj)
