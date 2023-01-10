7e919cc37c2f2fdd8c3d2eb5ea1f50dee3ababfb
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-10 19:54:34 +0000

    Create 4-... files

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""From JSON string to Object"""
+import json
+
+
+def from_json_string(my_str):
+    """returns an object represented by a JSON string
+    Args:
+        my_str (string): JSON string
+    Return:
+        an object represented by a JSON string
+    """
+    return json.loads(my_str)
