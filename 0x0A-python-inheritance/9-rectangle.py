f6d53c83eb358c2b612d9916cb746353923c0f1b
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-10 03:58:32 +0000

    Create 9-... files

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Rectangle class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class which inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """initializes a new Rectangle
+        Args:
+            width (int): rectangle width
+            height (int): rectangle height
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Calculates area of the rectangle
+        Return:
+            the area
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[{str(self.__class__.__name__)}] {self.__width}/{self.__height}"
