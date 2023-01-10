5b114681da22e7ba168caa569a7045329db93490
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-10 05:26:29 +0000

    Create 100-... files

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+MyInt a rebel class
+"""
+
+class MyInt(int):
+    """MyInt inherits from int but inverses the equal and not equal functions"""
+
+    def __eq__(self, value):
+        """invert this to be not equal to"""
+        return self.real != value
+
+    def __ne__(self, value):
+        """invers to be equal to"""
+        return self.real == value
