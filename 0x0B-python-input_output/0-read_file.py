3d630eb3c07d455700f6826f5d5c2901500c46e8
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-10 16:14:20 +0000

    Create 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" read file """
+
+
+def read_file(filename=""):
+    """reads a text file and prints it to stdout
+    Args:
+        filename (strin): name of file
+    """
+    with open(filename, encoding='utf-8') as file:
+        data = file.read()
+        print(data)
