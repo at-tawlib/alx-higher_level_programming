066c3813a58c3af1d2f9c1109c41d66302eaee5c
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-10 16:20:44 +0000

    Update file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,11 +2,10 @@
 """ read file """
 
 
-def read_file(filename="", encoding='utf8'):
+def read_file(filename=""):
     """reads a text file and prints it to stdout
     Args:
         filename (strin): name of file
     """
     with open(filename, encoding='utf-8') as file:
-        data = file.read()
-        print(data)
+        print(file.read(), end="")
