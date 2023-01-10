1d5c80b91d3f6e283e11a2af7ec26f9784b09327
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-10 05:45:41 +0000

    Pycodestyle complaint

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Add new attribute to a class"""
+
 
 def add_attribute(obj, att, value):
+
     """Adds a new attribute to an oject if possible
     Args:
         obj: object to add attribute to
