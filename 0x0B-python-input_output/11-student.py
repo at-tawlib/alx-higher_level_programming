ddd00aa64c12a044027e510f15a70791b8a5de80
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-11 05:21:48 +0000

    Create 11-... files

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Student class
+"""
+
+
+class Student:
+    """ Class student """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionary representation of Student"""
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for k, v in json.items():
+            setattr(self, k, v)
