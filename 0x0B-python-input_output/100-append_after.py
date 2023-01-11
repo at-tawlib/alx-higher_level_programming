8a3f5c853bd00d0a6c2676ecc18f966dff45020c
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-01-11 05:57:27 +0000

    Create file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""search and update file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file, after each line containing
+    a specific string"""
+    text = ""
+    with open(filename) as file:
+        for line in file:
+            text += line
+            for search_string in line:
+                text += new_string
+    with open(filename, 'w') as write_file:
+        write_file.write(text)
