197fe509b8482dcad51b0be378c8739d5f0961e3
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-02-16 05:15:39 +0000

    Update file

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,4 @@
 -- each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
 
-SELECT cities.id, cities.name, states.name FROM cities JOIN states WHERE cities.state_id = states.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id;
