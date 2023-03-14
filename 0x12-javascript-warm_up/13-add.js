b3272d929cca26d0e6ed6fcf6410c2d5e89a6126
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-14 05:40:41 +0000

    Create 13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+ * a function that returns the addition of 2 integers
+ * The function must be visible from outside
+ */
+exports.add = function (a, b) {
+  return parseInt(a) + parseInt(b);
+};
