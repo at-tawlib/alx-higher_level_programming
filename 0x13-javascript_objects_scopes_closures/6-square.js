0ccf3635412236738e88c9d19cd87ae41ee04fa3
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-15 02:26:03 +0000

    Create 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const Square1 = require('./5-square.js');
+
+/*
+ * class Square that defines a square and inherits from Square of 5-square.js:
+ * has an instance method called charPrint(c) that prints the rectangle using the character c
+ * If c is undefined, use the character X
+ */
+class Square extends Square1 {
+  constructor (size) {
+    super();
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
+}
+
+module.exports = Square;
