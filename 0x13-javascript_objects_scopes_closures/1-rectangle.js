1eca93cde8695ecad376dc787c47cddd53bdd93c
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-15 02:15:37 +0000

    Create 1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+ * Rectangle that defines a Rectangle
+ * The constructor must take 2 arguments w and h
+ * Initialize the instance attribute width with the value of w
+ * Initialize the instance attribute height with the value of h
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
