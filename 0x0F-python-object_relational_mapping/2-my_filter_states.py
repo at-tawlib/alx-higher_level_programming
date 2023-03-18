91856295b392d57c3661a8acd3fc10188c5216da
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-18 07:11:13 +0000

    Create 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    the script should take 4 arguments: mysql username, mysql password,
+    database name and state name searched (no argument validation needed)
+    results must be sorted in ascending order by states.id
+    """
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    name = argv[4]
+
+    connection = MySQLdb.connect(host, user, password, database_name, port)
+    cursor = connection.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name = %s
+            ORDER BY id;""", (name, ))
+    rows = cursor.fetchall()
+    connection.close()
+
+    for row in rows:
+        print(row)
