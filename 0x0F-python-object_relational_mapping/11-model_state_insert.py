166e3bccaf9083bb0aa5b75023874d1cec87b19a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-19 22:59:32 +0000

    Create 11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """
+    script takes 3 arguments: mysql username, mysql password and database name
+    script should connect to a MySQL server running on localhost at port 3306
+    Print the new states.id after creation
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+
+    session.add(new_state)
+    session.commit()
+    session.close()
