ff56cc6d6405e0c7ecaa937477a6c103603ae7e2
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-19 16:57:38 +0000

    Add order_by to the query

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).first()
+    state = session.query(State).order_by(State.id).first()
 
     if state is not None:
         print(f"{state.id}: {state.name}")
