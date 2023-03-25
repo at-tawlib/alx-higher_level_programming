6db677b0082c45e7eac11bde0df8b49c69026c89
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-25 03:44:57 +0000

    Create 100-relationship_states_cities.py

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+creates state California with City San Francisco
+"""
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    script should take 3 arguments: mysql username,
+    mysql password and database name
+    script should connect to a MySQL server running on localhost at port 3306
+    must use the cities relationship for all State objects
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
+
+    session.add(new_state)
+    session.commit()
+    session.close()
