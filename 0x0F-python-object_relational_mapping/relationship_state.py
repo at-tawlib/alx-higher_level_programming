29d73b23d56ade441f6d8ad66894dc2e0d8354d2
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-25 03:44:49 +0000

    Create relationship files

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+contains class definition of a State and an instance of Base
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ State class"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = Relationship('City', backref="state", cascade="all, delete")
