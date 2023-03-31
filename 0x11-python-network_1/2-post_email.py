c1dec6bfca149080c4fe3412bc562eeeb9194fb4
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-31 16:43:25 +0000

    Create 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+
+    data = {'email': sys.argv[2] }
+    request = urllib.request.Request(sys.argv[1], data=data)
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print(body)
+
