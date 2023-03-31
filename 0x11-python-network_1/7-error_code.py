b0b56de8ac1da5466dfc4dca6922ceb96d6d2fef
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-31 20:54:31 +0000

    Create 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+ takes in a URL, sends a request to the URL and displays the body
+ of the response (decoded in utf-8)
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    req = requests.get(sys.argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.content().decode('utf-8'))
