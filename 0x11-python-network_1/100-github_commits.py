854226785a5a7f81393c4d6d5228bbd748669511
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-01 05:47:08 +0000

    Add try except block

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,7 +13,10 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo_name)
     response = requests.get(url)
     commits = response.json()
-    for i in range(10):
-        print("{}: {}".format(
-            commits[i].get('sha'),
-            commits[i].get("commit").get('author').get('name')))
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get('sha'),
+                commits[i].get("commit").get('author').get('name')))
+    except IndexError:
+        pass
