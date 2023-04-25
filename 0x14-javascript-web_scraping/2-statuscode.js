178c29f5a9f67318aac08c5690e0eaffdfb483d8
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-25 15:04:02 +0000

    Create 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// displays the status code of a GET request
+// first argument is URL to the request
+
+const request = require('request');
+
+const url = process.argv[2];
+request(url).on('response', function (response) {
+  console.log(response.statusCode);
+});
